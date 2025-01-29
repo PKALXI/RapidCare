3bff20cc0fcc2ed97ddb87704e1221bd7692663c
Inreet-kaur <reetkhehra11@gmail.com>
2025-01-28 22:01:49 -0500

    general.

--- src/rapidcare-frontend/src/index.tsx
+++ src/rapidcare-frontend/src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
+import App from './App';
 import reportWebVitals from './reportWebVitals.js';
 import { Provider } from 'react-redux';
-import store from './store';
-import App from './App';
+import store from './redux/store';
+
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);

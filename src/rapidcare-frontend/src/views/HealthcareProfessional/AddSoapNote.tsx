aa704996b48e20c405d176e1ab8f9ae935410efe
Gurleen Kaur Rahi <rahig@mcmaster.ca>
2025-02-05 01:27:14 -0500

    fixed connection

--- src/rapidcare-frontend/src/views/HealthcareProfessional/AddSoapNote.tsx
+++ src/rapidcare-frontend/src/views/HealthcareProfessional/AddSoapNote.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Card, CardContent, CardHeader, Typography, Modal, Box, IconButton, Button, Grid } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
-import { useNavigate } from "react-router-dom";
 
 interface AddSoapNoteProps {
   open: boolean;
@@ -11,22 +10,23 @@ interface AddSoapNoteProps {
 
 const AddSoapNote: React.FC<AddSoapNoteProps> = ({ open, setOpen, patientId }) => {
 
-    const navigate = useNavigate();
-    const handleCloseAndNavigate = () => {
-        setOpen(false); // Close modal
-        navigate("/SoapView"); // Navigate to SoapView
-      };
-
     return (
         <div>
           <Modal open={open} onClose={() => setOpen(false)}>
                 <Box className="w-3/4 mx-auto mt-64 bg-white p-4 rounded relative">
                     <div className="p-4 flex justify-between items-center">
                         <Typography variant="h6">New Soap Note</Typography>
-                        <IconButton onClick={handleCloseAndNavigate}>
+                        <IconButton onClick={() => setOpen(false)}>
                             <CloseIcon />
                         </IconButton>
-                    </div>               
+                    </div>
+                  
+
+
+
+
+
+                    
                 </Box>
             </Modal>
         </div>
